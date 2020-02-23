6eff26a708c67fd5ca987b5c4a88ce024044a553
Omid Nassir <nassiromid@gmail.com>
2020-02-23 12:00:02 -0800

    Added contructors, getters/setters

--- src/main/java/com/rissandimo/studentcms/dao/Student.java
+++ src/main/java/com/rissandimo/studentcms/dao/Student.java
@@ -1,15 +1,31 @@
 package com.rissandimo.studentcms.dao;
 
-import lombok.Data;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-
-@Data
 @Entity
 public class Student extends SchoolPersonnel
 {
-    @OneToMany
-    private List<Course> courseList;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+                            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private Set<Course> courseList = new HashSet<>();
+
+    public Set<Course> getCourseList()
+    {
+        return courseList;
+    }
+
+    public void addCourse(Course newCourse)
+    {
+        this.courseList.add(newCourse);
+    }
 }
