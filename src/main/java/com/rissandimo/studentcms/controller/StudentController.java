59b20c7ebf6cc22718c2053e0b6755d3006f1cc6
Omid Nassir <nassiromid@gmail.com>
2020-02-23 11:56:21 -0800

    Added Student, Course and Instructor controllers

---
+++ src/main/java/com/rissandimo/studentcms/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.rissandimo.studentcms.controller;
+
+import com.rissandimo.studentcms.dao.Student;
+import com.rissandimo.studentcms.exceptions.StudentNotFoundException;
+import com.rissandimo.studentcms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StudentController
+{
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> retrieveStudents()
+    {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable("id") int id)
+    {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if(studentOptional.isPresent())
+            return studentOptional.get();
+        else throw new StudentNotFoundException("Student id: " + id + " not found");
+    }
+
+    @PostMapping("/students")
+    public Student createStudent(@Valid @RequestBody Student student)
+    {
+        return studentRepository.save(student);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudentById(@PathVariable("id") int id)
+    {
+        studentRepository.deleteById(id);
+    }
+}
